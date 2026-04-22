dfecd9a56a897d26e2d71e5e4ef114b95c991454
thanh thao <you@example.com>
2026-04-23 00:39:08 +0700

    them chuc nang xoa

---
+++ report.py
@@ -0,0 +1,76 @@
+from tkinter import*
+from PIL import Image,ImageTK 
+from tkinter import ttk, messagebox
+import sqlite3
+class reportClass:
+    def _init_(self,root):
+        self.root = root
+        self. root.title("student result management system")
+        self.root.geometry("1200x480+80+170")
+        self.root.config(bg="white")
+        self.root.focus_force()
+        #title 
+        title= Label(self.root, text="View student result", font= ("Arial", 20, "bold"),bg="orange",fg="#262626").place(x=10,y=15,width=1180,height=50)
+        #search
+        self.var_search= StringVar()
+        lbl_search=Label(self.root,text="search by roll No.",font=("arial",20,"bold"),bg ="white").place(x=280, y = 100)
+        txt_search=Entry(self.root,textvariable=self.var_search,font=("arial",20),bg ="lightyellow").place(x=520, y= 100, width=150)
+        btn_search=Button(self.root,text='search', font =("arial",15,"bold"), bg="03a9f4",fg="white", cursor="hand2",command=self.search).place(x= 680,y= 100, width=100,height= 35)
+        btn_clear=Button(self.root,text='clear', font =("arial",15,"bold"), bg="gray",fg="white", cursor="hand2",command=self.clear).place(x= 680,y= 100, width=100,height= 35)
+
+        #result_label
+        lbl_roll=Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 150, y = 230, width= 150, height = 50)
+        lbl_name=Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 300, y = 230, width= 150, height = 50)
+        lbl_course=Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 450, y = 230, width= 150, height = 50)
+        lbl_marks =Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 600, y = 230, width= 150, height = 50)
+        lbl_full=Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 750, y = 230, width= 150, height = 50)
+        lbl_per =Label(self.root, text="Roll No", font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 900, y = 230, width= 150, height = 50)
+
+        self.roll=Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 150, y = 280, width= 150, height = 50)
+        self.name=Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 300, y = 280, width= 150, height = 50)
+        self.course=Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 450, y = 280, width= 150, height = 50)
+        self.marks =Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 600, y = 280, width= 150, height = 50)
+        self.full=Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 750, y = 280, width= 150, height = 50)
+        self.per =Label(self.root, font=("arial", 15, "bold"), bg="white", bd =2,relief= GROOVE).place(x = 900, y = 280, width= 150, height = 50)
+
+        #button delete
+        btn_delete=Button(self.root,text='delete', font =("arial",15,"bold"), bg="red",fg="white", cursor="hand2").place(x= 500,y= 350, width=100,height= 35)
+#=====================
+    def search(self):
+        con = sqlite3.connect(database="rms.db")
+        cur =con.cursor()
+        try:
+            if self.var_search.get()=="":
+                messagebox.showerror("error","roll no. should be required", parent=self.root)
+            else:
+                cur.execute("select * from result where roll=?",(self.var_search.get(),))
+                row =cur.fetchone()
+                if row!= None:
+                    self.var_id=row[0]
+                    self.roll.config(text=row[1])
+                    self.name.config(text=row[2])
+                    self.course.config(text=row[3])
+                    self.marks.config(text=row[4])
+                    self.full.config(text=row[5])
+                    self.per.config(text=row[6])
+                else:
+                    messagebox.showerror("Error","No record found", parent= self.root)
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to {str(ex)}")
+
+    def clear(self):
+        self.roll.config(text="")
+        self.name.config(text="")
+        self.course.config(text="")
+        self.marks.config(text="")
+        self.full.config(text="")
+        self.per.config(text="")
+        self.var_search.set("")
+
+    
+
+if __name__== "__main__":
+    root=Tk()
+    obj=reportClass(root)
+    root.mainloop()
